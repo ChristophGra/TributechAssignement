375dc2b3cbcc857f70c7ac1caedf12304ee451aa
ChristophGra <christoph.grarock@gmail.com>
2020-08-23 14:59:21 +0200

    Initial Commit

---
+++ Tributech Assignement/Connection/SocketConn.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace Tributech_Assignement.Connection
+{
+	public class SocketConn
+	{
+		private Socket _ConnectedSocket;
+	
+		private BackgroundWorker receiveWorker;
+		private BackgroundWorker sendWorker;
+		private Queue<byte[]> receivedQueue;
+		private Queue<byte[]> sendQueue;
+		private SocketConn() 
+		{
+			sendQueue = new Queue<byte[]>();
+			receivedQueue = new Queue<byte[]>();
+			receiveWorker = new BackgroundWorker();
+			receiveWorker.DoWork += ReceiveWorker_DoWork;
+		}
+		public IEnumerable<byte[]> GetMessage()
+		{
+			byte[] buf = null;
+			while (true)
+			{
+				lock (receivedQueue)
+				{
+					if (receivedQueue.Count > 0)
+						buf = receivedQueue.Dequeue();
+				}
+				if (buf != null)
+					yield return buf;
+				else
+					Thread.Sleep(10);
+			}
+		}
+		public void QueueMessage(byte[] message)
+		{
+			lock(sendQueue)
+			{
+				sendQueue.Enqueue(message);
+			}
+		}
+		private void ReceiveWorker_DoWork(object sender, DoWorkEventArgs e)
+		{
+			byte[] buffer = new byte[1024];
+			while(true)
+			{
+				if (_ConnectedSocket.Receive(buffer) > 0)
+				{
+					lock (receivedQueue)
+					{
+						receivedQueue.Enqueue(buffer);
+					}
+				}
+				else
+					Thread.Sleep(10);
+			}
+		}
+
+		public static IEnumerable<SocketConn> GetSocket(bool listener, int port, string hostAdress = "localhost")
+		{
+			if (listener)
+				return GetListeningSocket(port, hostAdress);
+			else
+				return GetInitiatingSocket(port, hostAdress);
+		}
+		public static IEnumerable<SocketConn> GetListeningSocket(int port, string hostAdress)
+		{
+			Socket tempsocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+			tempsocket.Bind(CreateEndPoint(port, hostAdress));
+			tempsocket.Listen(10);
+			while (true)
+			{
+				SocketConn connection = new SocketConn();
+				connection._ConnectedSocket = tempsocket.Accept();
+				yield return connection;
+			}
+		}
+		private static IPEndPoint CreateEndPoint(int port, string hostAddress  )
+		{
+			IPHostEntry host = Dns.GetHostEntry(hostAddress);
+			IPAddress ipAddress = host.AddressList[0];
+			IPEndPoint endPoint = new IPEndPoint(ipAddress, port);
+			return endPoint;
+		}
+		public static IEnumerable<SocketConn> GetInitiatingSocket(int port, string hostAdress)
+		{
+			var remoteEP = CreateEndPoint(port, hostAdress);
+			while (true)
+			{
+				SocketConn connection = new SocketConn();
+				connection._ConnectedSocket = new Socket(remoteEP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+				connection._ConnectedSocket.Connect(remoteEP);
+				yield return connection;
+			}
+		}
+	}
+}
---
+++ Tributech Assignement/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
+using Tributech_Assignement.Connection;
+
+namespace Tributech_Assignement
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length > 1)
+			{
+				if(int.TryParse(args[0], out int port) && bool.TryParse(args[1], out bool listening))
+				{
+					foreach (SocketConn sock in SocketConn.GetSocket(listening, port))
+					{
+						if (listening)
+						{
+							BackgroundWorker putDataInConsole = new BackgroundWorker();
+							putDataInConsole.DoWork += PutDataInConsole_DoWork;
+							putDataInConsole.RunWorkerAsync(new DoWorkEventArgs(sock));
+							while (putDataInConsole.IsBusy)
+							{
+								Thread.Sleep(10);
+							}
+						}
+						else
+						{
+							while (true)
+							{
+								sock.QueueMessage(Encoding.ASCII.GetBytes(Console.ReadLine()));
+							}
+						}
+					}
+				}
+			}
+		}
+
+		private static void PutDataInConsole_DoWork(object sender, DoWorkEventArgs e)
+		{
+			SocketConn socket = e.Argument as SocketConn;
+			if (socket != null)
+			{
+				foreach (byte[] buf in socket.GetMessage())
+				{
+					Console.Write(Encoding.ASCII.GetString(buf));
+				}
+			}
+		}
+	}
+}
